8c71db251cff883f26b876eb8d069407c839c226
ArtemRozh <artemrozh2@gmail.com>
2026-03-03 19:22:51 +0200

    [ADD] New component for all website configs [ADD] Routing list. Isnt implementing everywhere and its use is questionable. Eh.

---
+++ src/app/website-config/website-config.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-website-config',
+  imports: [],
+  templateUrl: './website-config.component.html',
+  styleUrl: './website-config.component.css'
+})
+export class WebsiteConfigComponent {
+
+}
